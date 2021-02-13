785676b606b423282a5c5d43e2323ea1b98f2534
Tony Lim <tonyisawesome@users.noreply.github.com>
2021-02-13 08:17:40 +0800

    Utility for detecting Chinese word

--- utils.py
+++ utils.py
@@ -8,6 +8,3 @@ def is_chinese(word: str) -> bool:
     Check if the word is Chinese
     """
     return True if chinese.search(word) else False
-
-
-# TODO: concatenate list of pinyin as in original sentence
