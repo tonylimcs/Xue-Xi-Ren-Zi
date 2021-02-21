992c4f318f0694b90f2b27a302d8e0134aba58f4
Tony Lim <tonyisawesome@users.noreply.github.com>
2021-02-22 04:07:10 +0800

    Font constants and functions

---
+++ gui/font/constants.py
@@ -0,0 +1,6 @@
+RED = 'red'
+GREEN = 'green'
+ORANGE = 'orange'
+
+STYLE = 'Arial'
+BODY_SIZE = 13
---
+++ gui/font/utils.py
@@ -0,0 +1,2 @@
+def format_text_color(text, color):
+    return f'<font color={color}>{text}</font>'
