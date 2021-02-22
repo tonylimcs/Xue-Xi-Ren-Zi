caea3783ae024c36f7586597c707a130901bf66d
Tony Lim <tonyisawesome@users.noreply.github.com>
2021-02-22 22:42:20 +0800

    Added method for splitting chinese characters

--- engine/utils.py
+++ engine/utils.py
@@ -10,3 +10,12 @@ def is_chinese(word: str) -> bool:
     :return: true if Chinese, else false
     """
     return True if chinese.search(word) else False
+
+
+def split_chinese(string: str) -> list:
+    """
+    Split a string of Chinese phrase into characters
+    :param string: Chinese phrase
+    :return: list of Chinese characters
+    """
+    return chinese.findall(string)
