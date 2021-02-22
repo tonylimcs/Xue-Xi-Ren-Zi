0b8643ed0ea14adcbb67e01feefc75fe615b8e59
Tony Lim <tonyisawesome@users.noreply.github.com>
2021-02-22 09:28:45 +0800

    Update io.py
    Added method for retrieving user database

--- engine/io.py
+++ engine/io.py
@@ -1,6 +1,24 @@
+import json
+
+
 def get_article(file_path):
     """
-    Return a list of lines retrieved from text file
+    Get content of the article
+    :param file_path: path of the article (.txt)
+    :return: list of lines from the article
     """
     with open(file_path, "r", encoding='utf-8') as f:
         return f.readlines()
+
+
+def get_user_db(file_path):
+    """
+    Get user database
+    :param file_path: path of the user database (.json)
+    :return: dictionary of user data
+    """
+    with open(file_path) as f:
+        return json.load(f)
+
+
+print(get_user_db("../database/user.json"))
