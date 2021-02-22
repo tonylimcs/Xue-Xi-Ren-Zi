1313684e68272d53e43392a545dd0f8755a66d82
Tony Lim <tonyisawesome@users.noreply.github.com>
2021-02-23 06:06:45 +0800

    Update pinyin_process.py
    Improved documentation

--- engine/pinyin_process.py
+++ engine/pinyin_process.py
@@ -6,9 +6,6 @@ english = re.compile(u'[A-Za-z]+( [A-Za-z]+)*')
 
 
 def preprocess(string: str, eng_data: list):
-    """
-    Remove English words on the input string
-    """
     eng_all = english.finditer(string)
     eng_words = set()
     for eng in eng_all:
@@ -22,10 +19,7 @@ def preprocess(string: str, eng_data: list):
     return string, eng_data
 
 
-def get_pinyin(data: list):
-    """
-    Retrieve pinyin from data
-    """
+def process(data: list):
     pinyin_full, pinyin_only, chars_only = [], [], []
     for tup in data:
         try:
@@ -42,40 +36,10 @@ def get_pinyin(data: list):
     return pinyin_full, pinyin_only, chars_only
 
 
-def num2dia(word: str) -> str:
-    """
-    Convert tone representation: numerical -> diacritical
-    """
-    if len(word) > 1 and re.match(r"\d{1,5}", word[-1]):
-        tone = int(word[-1])
-        if tone == 5:
-            # neutral tone
-            return word[:-1]
-
-        try:
-            # special case
-            pos = re.search('(?<=i)u', word).start()
-            return word[:pos] + dia[tone - 1][4] + word[pos + 1:-1]
-        except AttributeError:
-            pass
-
-        for i, v in enumerate(['a', 'e', 'i', 'o', 'u']):
-            try:
-                pos = re.search(v, word).start()
-                return word[:pos] + dia[tone - 1][i] + word[pos + 1:-1]
-            except AttributeError:
-                continue
-
-    return word
-
-
 def tokenize(pinyin_full: list, chars_only: list, eng_data: list) -> list:
-    """
-    Concatenate all pinyin and rejoin English words according to the original string
-    """
     tokens = []
     pos = 0
-    char_id = 0
+    idx = 0
     for i, obj in enumerate(pinyin_full):
         try:
             is_english = eng_data[0][0] == pos
@@ -84,21 +48,50 @@ def tokenize(pinyin_full: list, chars_only: list, eng_data: list) -> list:
         is_pinyin = type(obj) is tuple
 
         if is_english:
-            # rejoin removed English words
+            # append removed English words
             eng = eng_data.pop(0)
             eng_word = eng[1]
-
             tokens.append(eng_word)
             pos += len(eng_word)
 
         if is_pinyin:
+            # append Chinese characters
             for j in range(len(obj)):
-                tokens.append(chars_only[char_id])
+                tokens.append(chars_only[idx])
                 pos += 1
-                char_id += 1
+                idx += 1
         else:
-            # concatenate numbers or punctuations
+            # append numbers or punctuations
             pos += 1
             tokens.append(obj)
 
     return tokens
+
+
+def num2dia(pinyin: str) -> str:
+    """
+    Convert tone representation: numerical -> diacritical
+    :param pinyin: pinyin of a Chinese character
+    :return: diacritical format of the pinyin
+    """
+    if len(pinyin) > 1 and re.match(r"\d{1.txt,5}", pinyin[-1]):
+        tone = int(pinyin[-1])
+        if tone == 5:
+            # neutral tone
+            return pinyin[:-1]
+
+        try:
+            # special case
+            pos = re.search('(?<=i)u', pinyin).start()
+            return pinyin[:pos] + dia[tone - 1][4] + pinyin[pos + 1:-1]
+        except AttributeError:
+            pass
+
+        for i, v in enumerate(['a', 'e', 'i', 'o', 'u']):
+            try:
+                pos = re.search(v, pinyin).start()
+                return pinyin[:pos] + dia[tone - 1][i] + pinyin[pos + 1:-1]
+            except AttributeError:
+                continue
+
+    return pinyin
