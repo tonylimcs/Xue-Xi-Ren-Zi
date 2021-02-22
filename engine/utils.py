6e77705d03f63f25b63debf95651789af74e1c69
Tony Lim <tonyisawesome@users.noreply.github.com>
2021-02-22 09:59:20 +0800

    Update utils.py
    Improved documentation

--- engine/utils.py
+++ engine/utils.py
@@ -6,5 +6,7 @@ chinese = re.compile(u'[\u4e00-\u9fff]')
 def is_chinese(word: str) -> bool:
     """
     Check if the word is Chinese
+    :param word: a word/character
+    :return: true if Chinese, else false
     """
     return True if chinese.search(word) else False
