c836fdd6a1e5307b10c4c74f215737f96fe40a81
Tony Lim <cslim.careers@gmail.com>
2021-03-04 17:46:42 +0800

    Functions for updating user database in memory

--- model/data.py
+++ model/data.py
@@ -1,28 +1,56 @@
 from engine.processor import remove_english, process, tokenize
 from engine.filter import filter_pos
 
-from model.io import get_article, get_user_db
+from model.io import get_article, get_user_db, update_user_db
 from model.text_formatter import init_format
+
+USER_JSON = 'database/user.json'
 
 
 class Data:
 
     def __init__(self):
         self.tokens, py_list = [], []
-        lines = get_article('articles/1.txt')
+        lines = get_article('articles/test.txt')
         for count, line in enumerate(lines):
             print(f'{count + 1} of {len(lines)}')
-            line = line.strip('\n')
             line, eng_words = remove_english(line)
             py_full, py_only, chars_only = process(line)
             py_list += py_only
             self.tokens += tokenize(py_full, chars_only, eng_words)
-            self.tokens += '<br><br>'
+            self.tokens += ["<br><br>"]
+        self.tokens.pop()   # No need to break line for the last line
 
-        self.user = get_user_db('database/user.json')
+        self.user = get_user_db(USER_JSON)
         unseen_pos, self.learning_pos = filter_pos(self.user, self.tokens, py_list)
-        # Update user_db.json file
+        update_user_db(USER_JSON, self.user)
         self.text = init_format(self.tokens, learning=self.learning_pos, unseen=unseen_pos)
+
+    def update_counter(self, char, pinyin, is_correct):
+        if is_correct:
+            self.user["learning"][char][pinyin] -= 1
+            counter = self.user["learning"][char][pinyin]
+            print(f"**Correct!\n{char}({pinyin}): {counter}")
+
+            if counter == 0:
+                self.update_learned(char, pinyin)
+        else:
+            self.user["learning"][char][pinyin] = self.user["repeat"]  # Reset counter
+            counter = self.user["learning"][char][pinyin]
+            print(f"**Wrong. Reset counter.\n{char}[{pinyin}]: {counter}")
+
+    def update_learned(self, char, pinyin):
+        if char not in self.user["learned"]:
+            # Initialize
+            self.user["learned"][char] = []
+
+        if pinyin not in self.user["learned"][char]:
+            self.user["learned"][char].append(pinyin)
+
+        # Update "learning"
+        del self.user["learning"][char][pinyin]
+        if len(self.user["learning"][char]) == 0:
+            del self.user["learning"][char]
 
 
 data = Data()
