295f1f6f7fd75cde8d0e9e0f80ac647edd7f950b
Tony Lim <cslim.careers@gmail.com>
2021-03-04 14:54:35 +0800

    Bug fix

--- engine/checker.py
+++ engine/checker.py
@@ -6,7 +6,7 @@ def check_correct(user_input: str, learning: list, tokens: list):
     cur_pos, cur_pinyin = cur[0], cur[1]
     cur_char = tokens[cur_pos]
 
-    if not re.match(r'\d', user_input[-1]):
+    if len(user_input) > 0 and not re.match(r'\d', user_input[-1]):
         user_input += '5'
 
     result = user_input == cur_pinyin
