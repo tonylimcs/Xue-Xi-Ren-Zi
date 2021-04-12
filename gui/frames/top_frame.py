b6198c3cedf5fa3e27a847fa6247fe4e3b57842c
Tony Lim <cslim.careers@gmail.com>
2021-04-12 18:21:13 +0800

    Refactor upper_frame to top_frame

---
+++ gui/frames/top_frame.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+
+from gui.frames.top.body import Body
+from gui.frames.top.side_column import SideColumn
+
+
+class TopFrame(QFrame):
+    def __init__(self):
+        super().__init__()
+
+        self.top_layout = QVBoxLayout()
+
+        self.body = Body()
+        self.side_col = SideColumn()
+
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter.addWidget(self.body)
+        self.splitter.addWidget(self.side_col)
+        self.splitter.setHandleWidth(10)
+
+        self.top_layout.addWidget(self.splitter)
